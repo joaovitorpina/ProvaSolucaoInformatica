3e52d168edbdac2342049b663d0ed93431d152ab
João Vitor Goncalves Pina <joaovitorpinag@hotmail.com>
2020-08-10 20:04:05 -0300

    Finalizacão do projeto

--- ProvaSolucaoInformatica/Models/ErrorViewModel.cs
+++ ProvaSolucaoInformatica/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace ProvaSolucaoInformatica.Models
 {
     public class ErrorViewModel
--- ProvaSolucaoInformatica/Startup.cs
+++ ProvaSolucaoInformatica/Startup.cs
@@ -1,13 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProvaSolucaoInformatica.Databases.Default;
+using ProvaSolucaoInformatica.Services;
 
 namespace ProvaSolucaoInformatica
 {
@@ -20,28 +20,41 @@ namespace ProvaSolucaoInformatica
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var optionsBuilder = new DbContextOptionsBuilder<DefaultDbContext>();
+
+#warning Insira a connection string antes de iniciar
+            optionsBuilder.UseMySql("Insira connection string aqui");
+
+            services.AddTransient(s => new GerenciadorUsuarios(new DefaultDbContext(optionsBuilder.Options)));
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                options =>
+                {
+                    options.LoginPath = "/Conta/Login";
+                    options.LogoutPath = "/Conta/Deslogar";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(1);
+                });
+
             services.AddControllersWithViews();
+
+#warning Insira a connection string antes de iniciar
+            services.AddDbContextPool<DefaultDbContext>(builder =>
+                builder.UseMySql("Insira connection string aqui"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
             else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseAuthentication();
             app.UseRouting();
 
             app.UseAuthorization();
@@ -49,8 +62,8 @@ namespace ProvaSolucaoInformatica
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Index}/{action=Index}/{id?}");
             });
         }
     }
